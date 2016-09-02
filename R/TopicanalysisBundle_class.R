7eaa18f419a3b2b7d1d6a08f1d7f4ffa08438c1d
Andreas Blaette <andreas.blaette@uni-due.de>
2016-09-02 18:25:43 +0200

    swithing to Dell

--- R/TopicanalysisBundle_class.R
+++ R/TopicanalysisBundle_class.R
@@ -3,15 +3,29 @@
 #' @importFrom reshape2 melt
 #' @importFrom lsa cosine
 #' @import data.table
+#' @field foo something
 #' @examples 
 #' \dontrun{
-#' B <- TopicanalysisBundle$new(topicmodels = TAs)
-#' B$makeMatrix(big = T)
-#' saveRDS(B$termTopicMatrix, file="/Users/blaette/Lab/repos/diffusion/matrix.RData")
-#' B$termTopicMatrix <- readRDS("/Users/blaette/Lab/repos/diffusion/matrix.RData")
-#' B$similarity <- readRDS("/Users/blaette/Lab/repos/diffusion/simil.RData")
-#' B$doClustering()
-#' B$getClusters(h=1)
+#' source("/Users/blaette/Lab/repos/diffusion/code/01_global_variables.R")
+#' B <- TopicanalysisBundle$new()
+#' B$filenames <- filenames
+#' B$getTerms()
+#' B$dist <- readRDS("/Users/blaette/Lab/repos/diffusion/data/n_topics_250/dist_binary_take2.RData")
+#' colnames(B$dist) <- colnames(B$terms)
+#' rownames(B$dist) <- colnames(B$terms)
+#' B$dist <- as.dist(B$dist)
+#' B$hclust <- hclust(B$dist, method="ward.D")
+#' B$hclust$height <- B$hclust$height[order(B$hclust$height)]
+#' png("/Users/blaette/Lab/tmp/dendrogram.png", width = 30000, height = 30000)
+#' plot(as.dendrogram(B$hclust), horiz = T, cex = 0.01)
+#' dev.off()
+#' 
+#' B$getClusters(h = 1.2)
+#' names(B$clusters)[as.integer(names(B$query("Integration")[1]))] <- "Integration"
+#' 
+#' B$restore()
+#' B$assignLabels()
+#' 
 #' }
 #' @export TopicanalysisBundle
 TopicanalysisBundle <- R6Class(
@@ -24,9 +38,13 @@ TopicanalysisBundle <- R6Class(
     
     filenames = NULL,
     
+    terms = NULL,
+    
     termTopicMatrix = NULL,
     
     simil = NULL,
+    
+    dist = NULL,
     
     hclust = NULL,
     
@@ -35,6 +53,85 @@ TopicanalysisBundle <- R6Class(
     length = function() length(self$topicmodels),
     
     names = function() unname(sapply(self$topicmodels, function(x) x$name)),
+    
+    restore = function(x=NULL, verbose = TRUE){
+      "restore it"
+      if (is.null(x)) x <- names(self$filenames)
+      B$topicmodels <- lapply(
+        setNames(x, x),
+        function(what){
+          if (file.exists(self$filenames[[what]]["lda"])){
+            if (verbose) message("... restoring: ", what)
+            TA <- Topicanalysis$new(
+              topicmodel = readRDS(self$filenames[[what]]["lda"])
+            )
+          } else {
+            if (verbose) warning("restoring ", what, " failed - topicmodel file not available")
+          }
+          if (file.exists(self$filenames[[what]]["labels"])){
+            TA$labels = readRDS(self$filenames[[what]]["labels"])
+            if (verbose) message("[labels: OK]")
+          } else {
+            if (verbose) message("[labels: NOT AVAILABLE]")
+          }
+          if (file.exists(self$filenames[[what]]["exclude"])){
+            TA$exclude = readRDS(self$filenames[[what]]["exclude"])
+            if (verbose) message("[exclude: OK]")
+          } else {
+            if (verbose) message("[exclude: NOT AVAILABLE]")
+          }
+          TA
+        }
+      )
+    },
+    
+    store = function(verbose = TRUE){
+      lapply(
+        names(self$topicmodels),
+        function(topicmodel_name){
+          if ("labels" %in% names(self$filenames[[topicmodel_name]])){
+            saveRDS(self$topicmodels[[topicmodel_name]]$labels, file=self$filenames[[topicmodel_name]]["labels"])
+          } else {
+            message("labels not saved for, ", topicmodel_name, " (no filename available)")
+          }
+          if ("exclude" %in% names(self$filenames[[topicmodel_name]])){
+            saveRDS(self$topicmodels[[topicmodel_name]]$exclude, file=self$filenames[[topicmodel_name]]["exclude"])
+          } else {
+            message("exclude vector not saved for, ", topicmodel_name, " (no filename available)")
+          }
+        }
+      )
+    },
+    
+    getTerms = function(k = 100, verbose = TRUE){
+      Ms <- lapply(
+        names(self$filenames),
+        function(id){
+          if (verbose) message("... loading and processing topicmodel: ", id)
+          topicmodel <- readRDS(file = self$filenames[[id]]["lda"])
+          M <- topicmodels::terms(topicmodel, k = k)
+          colnames(M) <- paste(id, c(1:ncol(M)), sep="::")
+          M
+        }
+      )
+      if (verbose) message("... putting together simple_triplet_matrix")
+      M <- do.call(cbind, Ms)
+      topicsVector <- unlist(lapply(Ms, colnames))
+      rm(Ms)
+      topicsIndex <- setNames(c(1:length(topicsVector)), topicsVector)
+      uniqueTerms <- unique(as.vector(M))
+      termIndex <- setNames(c(1:length(uniqueTerms)), uniqueTerms)
+      self$terms <- slam::simple_triplet_matrix(
+        i = unname(termIndex[as.vector(M)]),
+        j = rep(1:ncol(M), each = nrow(M)),
+        v = as.vector(apply(M, 2, function(x) termIndex[x])),
+        ncol = length(topicsIndex),
+        nrow = length(termIndex),
+        dimnames=list(names(termIndex), names(topicsIndex))
+      )
+      invisible(self$terms)
+    },
+    
     
     makeTermTopicMatrix = function(output = "matrix", minimize = FALSE, verbose = TRUE, ...){
       stopifnot(output %in% c("matrix", "simple_triplet_matrix", "big.matrix"))
@@ -72,7 +169,7 @@ TopicanalysisBundle <- R6Class(
           c(1:length(self$filenames)),
           function(i){
             setTxtProgressBar(pb, value=i)
-            M <- readRDS(self$filenames[i])
+            M <- readRDS(self$filenames[[i]])
             list(terms=M@terms, k=M@k)
           }
         )
@@ -154,12 +251,24 @@ TopicanalysisBundle <- R6Class(
     
     similarity = function(...){
       message("... calculating cosine similarity")
-      self$simil <- polmineR.misc::similarity(self$termTopicMatrix...)
+      self$simil <- polmineR.misc::similarity(self$termTopicMatrix, ...)
       invisible(self$simil)
     },
     
+    distance = function(chunks = 1, method="binary", mc = FALSE, progress = TRUE, verbose = TRUE){
+      if (is.null(self$terms) || class(self$terms) != "simple_triplet_matrix"){
+        stop("object of class 'simple_triplet_matrix' needs to be available in field terms")
+      }
+      if (chunks == 1){
+        self$dist <- proxy::dist(as.matrix(self$terms), method = method, by_rows = FALSE)
+      } else {
+        self$dist <- blapply(as.matrix(self$terms), f = proxy::dist, method = method, chunks = chunks, by_rows = F, mc = mc, progess = progress, verbose = FALSE)
+      }
+      invisible(self$dist)
+    },
+    
     doClustering = function(){
-      self$hclust <- hclust(as.dist(proxy::pr_simil2dist(self$similarity)), method="ward.D")
+      self$hclust <- hclust(as.dist(proxy::pr_simil2dist(self$simil)), method="ward.D")
     },
     
     plot = function(...){
@@ -168,12 +277,16 @@ TopicanalysisBundle <- R6Class(
     
     getClusters = function(h, k=25){
       groups <- cutree(self$hclust, h=h)
-      groupList <- split(x = names(groups), f = unname(groups))
-      .getTerms <- function(topics, ...){
-        M <- self$termTopicMatrix[,topics]
-        apply(M, 2, function(column) rownames(M)[order(column, decreasing = TRUE)][1:k])
-      }
-      self$clusters <- blapply(x = groupList, f = .getTerms, progress=TRUE, mc=FALSE)
+      clusterList <- split(x = names(groups), f = unname(groups))
+      M <- lapply(split(self$terms$v, self$terms$j), function(x) self$terms$dimnames[[1]][x])
+      M <- do.call(cbind, M)
+      colnames(M) <- colnames(self$terms)
+      # .getTerms <- function(topics, ...){
+      #   select <- self$terms[, topics]
+      #   cols <- lapply(split(select$v, select$j), function(x) self$terms$dimnames[[1]][x])
+      #   do.call(cbind, cols)
+      # }
+      self$clusters <- lapply(clusterList, function(x) M[,x])
     },
     
     label = function(min = 0, max = 12){
@@ -199,6 +312,39 @@ TopicanalysisBundle <- R6Class(
             function(id) self$topicmodels[[id[1]]]$labels[as.integer(id[2])] <- label
           )
         })
+    },
+    
+    query = function(query){
+      noHits <- sapply(
+        setNames(self$clusters, c(1:length(self$clusters))),
+        function(x) sum(table(as.vector(x))[query], na.rm = TRUE)
+      )
+      if (length(which(noHits == 0))) noHits <- noHits[-which(noHits == 0)]
+      noHits[order(noHits, decreasing = TRUE)]
+      
+    },
+    
+    as.zoo = function(x, y=NULL, k = 3, aggregation = "year"){
+      ts_list <- lapply(
+        setNames(names(self$topicmodels), names(self$topicmodels)),
+        function(id) {
+          if (x %in% self$topicmodels[[id]]$labels){
+            message("... getting time series data for: ", id)
+            count <- self$topicmodels[[id]]$as.zoo(x = label, y = y, aggregation = aggregation)  
+            DT <- data.table(date = index(count), as.data.frame(count))
+            DT[, "id" := id, with = TRUE]
+            return(DT)
+          } else {
+            return(NULL)
+          }
+          
+        }
+      )
+      ts <- rbindlist(ts_list)
+      tab <- dcast.data.table(ts, date~id, value.var = "count", fun.aggregate = sum)
+      dates <- tab[["date"]]
+      tab[, "date" := NULL, with = TRUE]
+      zooObject <- zoo(x=tab, order.by = dates)
     }
   )
 )
