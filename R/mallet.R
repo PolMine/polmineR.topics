400276c60241a1fc4e7213f96d0bd77cc79da8f3
Andreas Blaette <andreas.blaette@uni-due.de>
2019-10-28 10:43:31 +0100

    mallet process consolidated_

--- R/mallet.R
+++ R/mallet.R
@@ -1,11 +1,12 @@
 #' Interface to mallet topicmodelling.
 #' 
-#' The envisaged workflow will be as follows (see example):
-#' (1) Turn \code{partition_bundle}-object into mallet instance list, (2a) store the
-#' resulting \code{jobjRef}-object locally using \code{java_object_store}, create mallet command
-#' with \code{mallet_cmd}, and run mallet from the command line; alternativelly (2b) run
-#' mallet topic modelling from within R.
+#' Functionality to support the following workflow (see examples): (a) Turn
+#' \code{partition_bundle}-object into mallet instance list, (b) store the
+#' resulting \code{jobjRef}-object, (c) run mallet topic modelling and (d)
+#' turn ParallelTopicModel Java object into \code{LDA_Gibbs} object from
+#' package \code{topicmodels}.
 #' 
+#' @param x A \code{partition_bundle} object.
 #' @param terms_to_drop stopwords
 #' @param ... further parameters
 #' @param p_attribute The p_attribute to use, typically "word" or "lemma".
@@ -18,36 +19,51 @@
 #' @importFrom parallel mclapply
 #' @importFrom polmineR get_token_stream
 #' @examples  
+#' # Preparations: Create instance list
+#' 
 #' polmineR::use("polmineR")
 #' speeches <- polmineR::as.speeches("GERMAPARLMINI", s_attribute_name = "speaker")
 #' 
 #' if (requireNamespace("rJava")){
+#'   # options(java.parameters = "-Xmx4g")
 #'   library(rJava)
-#'   instance_list <- mallet_make_instance_list(speeches)
+#'   .jinit()
+#'   # We need to put the jars from mallet 2.0 on the classpath because
+#'   # only the newer mallet version (not the one included in mallet R package)
+#'   # has method ParallelTopicModel$getDocumentTopics() needed by as_LDA
+#'   .jaddClassPath("/opt/mallet-2.0.8/class") # after .jinit()
+#'   .jaddClassPath("/opt/mallet-2.0.8/lib/mallet-deps.jar")
+#'   instance_list <- topicanalysis::mallet_make_instance_list(speeches)
+#'   instancefile <- mallet_instance_list_store(instance_list)
 #' }
 #' 
-#' # Workflow 1: Run mallet from R using mallet package
+#' # Option 1: Run mallet from R using mallet package
 #' 
 #' if (requireNamespace("mallet")){
-#'   engine <- mallet::MalletLDA(num.topics = 20)
-#'   engine$loadDocuments(instance_list)
-#'   engine$setAlphaOptimization(20, 50)
-#'   engine$train(2000)
+#'   lda <- mallet::MalletLDA(num.topics = 20)
+#'   lda$loadDocuments(instance_list)
+#'   lda$setAlphaOptimization(20, 50)
+#'   lda$train(100)
 #' }
 #' 
-#' # Save instance list to disk and run mallet binary from command line
+#' # Option 2: Use ParallelTopicModel class - has write()-method
 #' 
-#' instancefile <- mallet_instance_list_store(instance_list, filename = tempfile()) # output to a tempfile
-#' destfile <- tempfile()
-#' cmd <- mallet_cmd(
-#'   mallet_bin_dir = "/opt/mallet-2.0.8/bin",
-#'   sourcefile = instancefile,
-#'   threads = 1,
-#'   destfile = destfile
-#' )
-#' system(cmd)
+#' if (requireNamespace("mallet")){
+#'   destfile <- tempfile()
+#'   lda <- rJava::.jnew("cc/mallet/topics/ParallelTopicModel", 25L, 5.1, 0.1)
+#'   lda$addInstances(instance_list)
+#'   lda$setNumThreads(1L)
+#'   lda$setTopicDisplay(50L, 10L)
+#'   lda$setNumIterations(150L)
+#'   lda$estimate()
+#'   lda$write(rJava::.jnew("java/io/File", destfile))
+#' }
+#' 
+#' # Load topicmodel and turn it into LDA_Gibbs
+#' 
 #' mallet_lda <- mallet_load_topicmodel(destfile)
-#' topicmodels_lda <- as(mallet_lda, "LDA_Gibbs")
+#' topicmodels_lda <- as_LDA(mallet_lda, "LDA")
+#' 
 #' @rdname mallet
 #' @importFrom polmineR get_token_stream
 #' @export mallet_make_instance_list
@@ -71,96 +87,45 @@ mallet_make_instance_list <- function(x, p_attribute = "word", terms_to_drop = t
   )
 }
 
-#' @details The \code{mallet_cmd} function will return a command to be executed
-#'   in a terminal that will prepare a topic model with the input parameters.
-#' @param x An object to process (HaHaHa).
-#' @param model A topic model prepared by mallet.
-#' @param mallet_bin_dir The directory where the executable 'mallet' resides.
-#' @param sourcefile File to which an instance list has been saved.
-#' @param destfile Directory where results will be saved.
-#' @param topwords ...
-#' @param topics The number of topics the topic model shall have.
-#' @param iterations Number of iterations to perform.
-#' @param threads Number of threads to use.
-#' @export mallet_cmd
+
+#' @details The \code{as_LDA()}-function will turn an estimated topic model
+#'   prepared using 'mallet' into a \code{LDA_Gibbs} object from the
+#'   \code{topicmodels} package.
+#' @export as_LDA
+#' @importFrom pbapply pblapply
 #' @rdname mallet
-mallet_cmd <- function(mallet_bin_dir, sourcefile, destfile, topwords = 50, topics = 50, iterations = 2000, threads = 1){
-  stopifnot(
-    is.numeric(topics), topics > 2,
-    is.numeric(iterations), iterations > 1,
-    is.numeric(topwords), topwords > 2,
-    is.numeric(threads), threads > 0
+as_LDA <- function(x, verbose = TRUE){
+  if (!grepl("ParallelTopicModel", x$getClass()$toString()))
+    stop("incoming object needs to be class ParallelTopicModel")
+  message("... getting topic probabilities")
+  gamma <- rJava::.jevalArray(x$getDocumentTopics(TRUE, TRUE), simplify = TRUE)
+  # gamma <- do.call(
+  #   rbind,
+  #   pblapply(0L:(x$data$size() - 1L), function(i) x$getTopicProbabilities(i))
+  # )
+  dimensions <- c(
+    x$data$size(), # Number of documents
+    x$getAlphabet()$size() # Number of terms
   )
-  
-  cmd <- c(
-    file.path(mallet_bin_dir, "mallet"), "train-topics",
-    "--input", sourcefile,
-    "--num-topics", topics,
-    "--num-iterations", iterations,
-    "--output-model", destfile,
-     "--num-threads", threads
-    )
-  paste(cmd, collapse = " ")
-}
-
-
-# setOldClass("jobjRef")
-
-#' @export
-setAs(from = "jobjRef", to = "LDA_Gibbs", function(from){
-  new(
+  message("... getting alphabet")
+  alphabet <- strsplit(x$getAlphabet()$toString(), "\n")[[1]]
+  message("... getting document names")
+  docs <- pblapply(0L:(x$data$size() - 1L), function(i) x$data$get(i)$instance$getName())
+  message("... preparing beta matrix")
+  beta <- rJava::.jevalArray(x$getTopicWords(TRUE, TRUE), simplify = TRUE) 
+  # beta <- t(as.matrix(mallet_get_word_weights(x)))
+  y <- new(
     "LDA_Gibbs",
-    Dim = c(
-      from$data$size(), # Number of documents
-      from$getAlphabet()$size() # Number of terms
-    ),
-    k = from$getNumTopics(),
-    terms = strsplit(from$getAlphabet()$toString(), "\n")[[1]],
-    documents = sapply(0L:(from$data$size() - 1L), function(i) from$data$get(i)$instance$getName()), # Vector containing the document names
-    beta = t(as.matrix(mallet_get_word_weights(from))), # A matrix; logarithmized parameters of the word distribution for each topic
-    gamma = do.call(rbind, lapply(0L:(from$data$size() - 1L), function(i) from$getTopicProbabilities(i))), # matrix, parameters of the posterior topic distribution for each document
-    iter = from$numIterations
+    Dim = dimensions,
+    k = x$getNumTopics(),
+    terms = alphabet,
+    documents = docs, # Vector containing the document names
+    beta = beta, # A matrix; logarithmized parameters of the word distribution for each topic
+    gamma = gamma, # matrix, parameters of the posterior topic distribution for each document
+    iter = x$numIterations
   )
-})
-
-
-
-#' @export mallet_get_word_weights
-#' @rdname mallet
-mallet_get_word_weights <- function(model){
-  if (!requireNamespace("rJava", quietly = TRUE)){
-    stop("Package 'rJava' required, but not available.")
-  }
-  destfile <- tempfile()
-  # file <- new(rJava::J("java/io/File"), destfile)
-  file <- rJava::.jnew("java/io/File", destfile)
-  # file_writer <- new(rJava::J("java/io/FileWriter"), file)
-  file_writer <- rJava::.jnew("java/io/FileWriter", file)
-  # print_writer <- new(rJava::J("java/io/PrintWriter"), file_writer)
-  print_writer <- rJava::new(rJava::J("java/io/PrintWriter"), file_writer)
-  model$printTopicWordWeights(print_writer)
-  print_writer$close()
-  mallet_load_word_weights(destfile)
+  y
 }
-
-
-#' @export mallet_load_word_weights
-#' @importFrom slam simple_triplet_matrix
-#' @rdname mallet
-mallet_load_word_weights <- function(filename){
-  word_weights_raw <- read.table(filename, sep = "\t")
-  # word_weights_raw <- subset(word_weights_raw, word_weights_raw[["V3"]] > min(word_weights_raw[,3]))
-  simple_triplet_matrix(
-    i = as.integer(word_weights_raw[,2]),
-    j = word_weights_raw[,1] + 1L,
-    v = word_weights_raw[,3],
-    dimnames = list(
-      levels(word_weights_raw[,2]),
-      as.character(1L:(max(word_weights_raw[,1]) + 1L))
-    )
-  )
-}
-
 
 
 #' @rdname mallet
@@ -178,3 +143,66 @@ mallet_instance_list_store <- function(x, filename = tempfile()){
 mallet_load_topicmodel <- function(filename){
   rJava::J("cc/mallet/topics/ParallelTopicModel")$read(rJava::.jnew("java/io/File", filename))
 }
+
+########################
+#                      #
+# Deprecated functions #
+#                      #
+########################
+
+# This function was used to import results before realizing that the 
+# data can be much more directly from the Java object.
+.mallet_get_word_weights <- function(model){
+  if (!requireNamespace("rJava", quietly = TRUE)){
+    stop("Package 'rJava' required, but not available.")
+  }
+  destfile <- tempfile()
+  file <- rJava::.jnew("java/io/File", destfile)
+  file_writer <- rJava::.jnew("java/io/FileWriter", file)
+  print_writer <- rJava::new(rJava::J("java/io/PrintWriter"), file_writer)
+  message("... printing topic word weights")
+  model$printTopicWordWeights(print_writer)
+  print_writer$close()
+  .mallet_load_word_weights(destfile)
+}
+
+
+# This function was used to import results before realizing that the 
+# data can be much more directly from the Java object.
+.mallet_load_word_weights <- function(filename){
+  message("... loading topic word weights")
+  word_weights_raw <- read.table(filename, sep = "\t")
+  # word_weights_raw <- subset(word_weights_raw, word_weights_raw[["V3"]] > min(word_weights_raw[,3]))
+  message("... creating simple triplet matrix")
+  simple_triplet_matrix(
+    i = as.integer(word_weights_raw[,2]),
+    j = word_weights_raw[,1] + 1L,
+    v = word_weights_raw[,3],
+    dimnames = list(
+      levels(word_weights_raw[,2]),
+      as.character(1L:(max(word_weights_raw[,1]) + 1L))
+    )
+  )
+}
+
+.mallet_cmd <- function(mallet_bin_dir, sourcefile, destfile, topwords = 50, topics = 50, iterations = 2000, threads = NULL){
+  stopifnot(
+    is.numeric(topics), topics > 2,
+    is.numeric(iterations), iterations > 1,
+    is.numeric(topwords), topwords > 2
+    #    , is.numeric(threads), threads > 0
+  )
+  
+  cmd <- c(
+    file.path(mallet_bin_dir, "mallet"), "train-topics",
+    "--input", sourcefile,
+    "--num-topics", topics,
+    "--num-iterations", iterations,
+    "--output-model", destfile,
+    if (is.null(threads)) c() else c("--num-threads", threads)
+  )
+  paste(cmd, collapse = " ")
+}
+
+
+
